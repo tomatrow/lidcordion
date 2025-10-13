f7d692a781a7dd4a3aa0528390c5d689a00bfb2a
AJ Caldwell <tomatrow@gmail.com>
2025-10-13 15:24:42 -0700

    Track velocity

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
 	import { Garmon } from "$lib/accordion"
 
-	let lidAngle = $state(-1)
+	let lidAngle = $state<number>()
+	let lastLidAngle = $state<number>()
+	const interval = 1000 / 60
+	let velocity = $state(0) // degrees/microsecond
+
+	$effect(() => {
+		const id = setInterval(() => {
+			velocity = ((lidAngle ?? 0) - (lastLidAngle ?? lidAngle ?? 0)) / interval
+			lastLidAngle = lidAngle
+		}, interval)
+
+		return () => {
+			clearInterval(id)
+		}
+	})
 
 	$effect(() => {
 		const eventSource = new EventSource("/api/lidangle")
 
 		eventSource.onmessage = (event) => {
 			const newLidAngle = parseFloat(event.data)
-			if (Number.isFinite(newLidAngle)) lidAngle = newLidAngle
+			if (Number.isFinite(newLidAngle) && 0 < newLidAngle && newLidAngle < 150)
+				lidAngle = newLidAngle
 		}
 
 		return () => {
@@ -18,5 +33,6 @@
 </script>
 
 <p>{lidAngle}</p>
+<p>{velocity}</p>
 
 <Garmon />
