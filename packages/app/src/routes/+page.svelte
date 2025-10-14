7b5136e3ab0ab96afc4fe0a005a2c079db741fba
AJ Caldwell <tomatrow@gmail.com>
2025-10-13 23:03:16 -0700

    Fiddle a bit more; round gain to avoid forever hovering around 0, make it feel more responsive by amplifying the gain and decreasing the tween duration

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -5,12 +5,14 @@
 	let lidAngle = $state<number>()
 	let lastLidAngle = $state<number>()
 	const interval = 1000 / 60
-	let velocity = new Tween(0) // degrees/microsecond
+	let velocity = new Tween(0, {
+		duration: 60
+	}) // degrees/microsecond(?)
 
 	$effect(() => {
 		const id = setInterval(() => {
 			const newVelocity = ((lidAngle ?? 0) - (lastLidAngle ?? lidAngle ?? 0)) / interval
-			velocity.target = Math.min(Math.max(-1, newVelocity), 1)
+			velocity.target = clamp(newVelocity, -1, 1)
 			lastLidAngle = lidAngle
 		}, interval)
 
@@ -33,7 +35,15 @@
 		}
 	})
 
-	let gain = $derived(Math.min(Math.abs(velocity.current), 1))
+	let gain = $derived(round(1.5 * Math.abs(velocity.current), 3))
+
+	function clamp(value: number, min: number, max: number) {
+		return Math.min(Math.max(value, min), max)
+	}
+
+	function round(value: number, place: number) {
+		return Math.floor(value * 10 ** place) / 10 ** place
+	}
 </script>
 
 <p>{lidAngle}</p>
