28bd46fcec5e155e1eb5baef6276b773d8bf890c
AJ Caldwell <tomatrow@gmail.com>
2025-10-18 19:45:31 -0700

    Add constant gain input

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -35,7 +35,8 @@
 	})
 
 	let gainScale = $state(1.5)
-	let gain = $derived(round(gainScale * Math.abs(velocity.current), 3))
+	let gainConstant = $state(0)
+	let gain = $derived(round(gainScale * Math.abs(velocity.current) + gainConstant, 3))
 
 	function clamp(value: number, min: number, max: number) {
 		return Math.min(Math.max(value, min), max)
@@ -52,7 +53,11 @@
 	<p><b>v<sub>c</sub></b> {round(velocity.current, 3)}</p>
 	<p><b>gain</b> {gain}</p>
 	<p>
-		<b>g<sub>c</sub></b> <input type="number" bind:value={gainScale} min={0} max={3} step={0.5} />
+		<b>g<sub>*</sub></b> <input type="number" bind:value={gainScale} min={0} max={3} step={0.5} />
+	</p>
+	<p>
+		<b>g<sub>+</sub></b>
+		<input type="number" bind:value={gainConstant} min={0} max={1} step={0.1} />
 	</p>
 </header>
 
